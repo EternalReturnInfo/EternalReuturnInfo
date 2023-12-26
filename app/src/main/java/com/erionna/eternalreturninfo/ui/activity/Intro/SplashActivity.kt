2c1d22601e0307837f2ec78d087b6678873dc24d
choco5732 <choco5732@gmail.com>
2023-12-27 07:31:10 +0900

    [FIX] : 안드로이드12부터 splash 화면이 2번 나오는 버그 발견, 버전별 splash화면 설정 변경

--- app/src/main/java/com/erionna/eternalreturninfo/ui/activity/Intro/SplashActivity.kt
+++ app/src/main/java/com/erionna/eternalreturninfo/ui/activity/Intro/SplashActivity.kt
@@ -1,6 +1,7 @@
 package com.erionna.eternalreturninfo.ui.activity.Intro
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -14,11 +15,23 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_activity)
 
-        Handler().postDelayed({
+        /**
+         * 안드로이드 12 버전 이상부터 SPLASH API 강제화로
+         * SPLASH 화면이 두 번 나오는 현상을 해결하기 위해
+         * 버전별 실행코드 달리함.
+         */
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+            Handler().postDelayed({
+                val intent = Intent(this, LoginActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                startActivity(intent)
+                finish()
+            },2000)
+        } else {
             val intent = Intent(this, LoginActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
             finish()
-        },2000)
+        }
     }
 }
