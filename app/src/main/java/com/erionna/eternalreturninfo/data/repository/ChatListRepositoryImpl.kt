80e29e9792fe35ba953258c2fd76f67853ea479c
choco5732 <choco5732@gmail.com>
2024-01-11 23:04:57 +0900

    [REFACTORING] : ChatList Repository 디렉토리 추가

---
+++ app/src/main/java/com/erionna/eternalreturninfo/data/repository/ChatListRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.erionna.eternalreturninfo.data.repository
+
+import com.erionna.eternalreturninfo.ui.repository.ChatListRepository
+
+class ChatListRepositoryImpl() : ChatListRepository {
+    override fun adduser() {
+        TODO("Not yet implemented")
+    }
+
+    override fun currentList() {
+        TODO("Not yet implemented")
+    }
+
+    override fun clearList() {
+        TODO("Not yet implemented")
+    }
+
+    override fun modifyItemForCallBack() {
+        TODO("Not yet implemented")
+    }
+
+    override fun modifyItemForChatList() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/erionna/eternalreturninfo/ui/repository/ChatListRepository.kt
@@ -0,0 +1,10 @@
+package com.erionna.eternalreturninfo.ui.repository
+
+interface ChatListRepository {
+    fun adduser()
+    fun currentList()
+    fun clearList()
+    fun modifyItemForCallBack()
+    fun modifyItemForChatList()
+
+}
